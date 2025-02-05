5b19f3de7a830a07a15e3e77080ffb5c691a0a04
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2025-02-05 21:12:34 +0800

    Delete backend directory

--- backend/routes/projex.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const projexController = require('../controller/projex.controller.js')
-
-router.get("/users", projexController.getAll)
-
-module.exports = router
